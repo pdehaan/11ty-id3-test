af1e22bceaaf994dc6469bf7e3467974e0e310a5
Peter deHaan <peter@deseloper.com>
2021-03-07 16:46:35 -0800

    Try music-metadata module

--- src/_data/mp3s.js
+++ src/_data/mp3s.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const globby = require("globby");
 const NodeID3 = require("node-id3");
+const mm = require("music-metadata");
 
 const baseDir = path.join(__dirname, "..", "assets");
 const mp3Path = path.join(baseDir, "*.mp3");
@@ -10,8 +11,11 @@ module.exports = async () => {
   const mp3s = await globby([mp3Path]);
   const res = [];
   for (const file of mp3s) {
-    const id3 = await NodeID3.read(file);
-    res.push({ file: file.replace(baseDir + path.sep, ""), ...id3 });
+    const { common } = await mm.parseFile(file);
+    const { picture, ...id3 } = common;
+
+    // const { image, raw, ...id3 } = await NodeID3.read(file);
+    res.push({ file: path.basename(file), ...id3 });
   }
   return res;
 };
